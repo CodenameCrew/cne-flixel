5d4ba0ba43c6f04de4ca39e34808408c3cb9205a
Redar13 <93761434+Redar13@users.noreply.github.com>
2024-03-27 18:30:00 +0500

    Fix negative scale
    At negative values of the scale hitbox is shifted in the wrong direction, and for what next to the edges of the screen sprite will not be drawn. I noticed it though, or someone else did lol.

--- flixel/FlxSprite.hx
+++ flixel/FlxSprite.hx
@@ -1384,8 +1384,9 @@ class FlxSprite extends FlxObject
 		newRect.setPosition(x, y);
 		if (pixelPerfectPosition)
 			newRect.floor();
-		_scaledOrigin.set(origin.x * scale.x, origin.y * scale.y);
-		_scaledFrameOffset.set(frameOffset.x * scale.x, frameOffset.y * scale.y);
+		_scaledOrigin.set(origin.x * Math.abs(scale.x), origin.y * Math.abs(scale.y));
+		_scaledFrameOffset.set(frameOffset.x * Math.abs(scale.x), frameOffset.y * Math.abs(scale.y));
+		// _scaledFrameOffset.set(frameOffset.x * scale.x, frameOffset.y * scale.y);
 		newRect.x += -Std.int(camera.scroll.x * scrollFactor.x) - offset.x + origin.x - _scaledOrigin.x;
 		newRect.y += -Std.int(camera.scroll.y * scrollFactor.y) - offset.y + origin.y - _scaledOrigin.y;
 		if (isPixelPerfectRender(camera))
