2dfe5dab804d379d5a8ee3446a8b45c53b75b10e
Ralty <78720179+Raltyro@users.noreply.github.com>
2026-03-01 20:20:52 +0700

    fix autopause

--- flixel/system/frontEnds/SoundFrontEnd.hx
+++ flixel/system/frontEnds/SoundFrontEnd.hx
@@ -353,7 +353,7 @@ class SoundFrontEnd
 	{
 		if (music != null && music.exists)
 		{
-			if (music._pausedPlay = music.playing) music.source.pause();
+			if (music._pausedPlay = music.source.playing) music.source.pause();
 			music._pausedByHandler = true;
 		}
 
@@ -361,7 +361,7 @@ class SoundFrontEnd
 		{
 			if (sound != null && sound.exists)
 			{
-				if (sound._pausedPlay = sound.playing) sound.source.pause();
+				if (sound._pausedPlay = sound.source.playing) sound.source.pause();
 				sound._pausedByHandler = true;
 			}
 		}
@@ -376,8 +376,8 @@ class SoundFrontEnd
 	{
 		if (music != null && music.exists && music._pausedByHandler)
 		{
-			if (music._pausedPlay) music.source.play();
 			music._pausedByHandler = false;
+			if (music._pausedPlay) music.source.play();
 			music._pausedPlay = false;
 		}
 
@@ -385,8 +385,8 @@ class SoundFrontEnd
 		{
 			if (sound != null && sound.exists && sound._pausedByHandler)
 			{
-				if (sound._pausedPlay) sound.source.play();
 				sound._pausedByHandler = false;
+				if (sound._pausedPlay) sound.source.play();
 				sound._pausedPlay = false;
 			}
 		}
